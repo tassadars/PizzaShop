9d85b93505695faf99ad14ebeac019f0d6016a3b
eugen <tassadars@gmail.com>
2015-11-12 18:04:34 +0200

    Add get_total_product_count func

--- public/app.js
+++ public/app.js
@@ -13,10 +13,23 @@ function something()
 function add_to_cart(id)
 {
 	var key = 'product_' + id;	
-	
+
 	var x = window.localStorage.getItem(key);
 	x = x*1 + 1;
 	window.localStorage.setItem(key, x);
 }
 
 
+function get_total_product_count ()
+{
+	var total = 0;
+
+	$.each(localStorage, function(key, value){
+  		if (key.indexOf("product_") > -1) {
+			total = total*1 + value*1;
+			//alert(value); 		
+  		}
+ 	});
+	
+	window.localStorage.setItem('total', total)
+}

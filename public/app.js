1bea9530a7278558ce8981189aa0dbe1aef14ff1
eugen <tassadars@gmail.com>
2015-11-13 11:11:25 +0200

    Add func get_all_cart_products()

--- public/app.js
+++ public/app.js
@@ -33,6 +33,19 @@ function get_total_product_count ()
 	window.localStorage.setItem('total', total)
 }
 
+function get_all_cart_products ()
+{
+	var order = '';
+
+	$.each(localStorage, function(k, v){
+  		if (k.indexOf(key) > -1) {
+			order = order + k + '=' + v + ',';
+  		}
+ 	});
+	
+	return order;
+}
+
 function get_product_count (id)
 {
 	//var key = window.localStorage.key(index); // get the key in hash 
@@ -41,7 +54,7 @@ function get_product_count (id)
 		else
 	return result; 	
 }
-
+ 
 /*
 function change(el)
 {
